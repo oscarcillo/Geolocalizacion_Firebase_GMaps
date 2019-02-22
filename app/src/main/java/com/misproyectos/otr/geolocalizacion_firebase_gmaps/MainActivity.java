63c5d32b64fbe39f7a97be9a301bf61079abd158
oscarcillo <oscar.tutor.rubio@gmail.com>
2019-02-22 20:56:06 +0100

    Subida de localización a la base de datos de Firebase

--- app/src/main/java/com/misproyectos/otr/geolocalizacion_firebase_gmaps/MainActivity.java
+++ app/src/main/java/com/misproyectos/otr/geolocalizacion_firebase_gmaps/MainActivity.java
@@ -1,13 +1,69 @@
 package com.misproyectos.otr.geolocalizacion_firebase_gmaps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    DatabaseReference dbReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        dbReference = FirebaseDatabase.getInstance().getReference();
+
+        uploadLocationFirebase();
+    }
+
+    private void uploadLocationFirebase() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Toast.makeText(MainActivity.this, "Latitud: "+location.getLatitude()
+                                    + " - Longitud: " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+
+                            Map<String, Object> latlang = new HashMap<>();
+                            latlang.put("latitud", location.getLatitude());
+                            latlang.put("longitud", location.getLongitude());
+                            dbReference.child("usuarios").push().setValue(latlang);
+                        }
+                    }
+                });
     }
 }
