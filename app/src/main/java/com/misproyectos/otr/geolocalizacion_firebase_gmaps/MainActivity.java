89a96e74a09052e7fd4970003a9eed97487ee40d
oscarcillo <oscar.tutor.rubio@gmail.com>
2019-02-22 21:11:07 +0100

    Creación de nueva actividad de Google Maps

--- app/src/main/java/com/misproyectos/otr/geolocalizacion_firebase_gmaps/MainActivity.java
+++ app/src/main/java/com/misproyectos/otr/geolocalizacion_firebase_gmaps/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private FusedLocationProviderClient fusedLocationClient;
     DatabaseReference dbReference;
 
+    private int MY_PERMISSION_REQUEST_LOCATION;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,13 +41,8 @@ public class MainActivity extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}
+            , MY_PERMISSION_REQUEST_LOCATION);
             return;
         }
 
